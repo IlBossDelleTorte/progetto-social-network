e53f329eaaabce15588c6b2427c327bb70b5f663
Hossam Lahbibate <hossambogos@gmail.com>
2019-10-14 17:00:03 +0200

    Embrione della classe bacheca

---
+++ social_network-VERSIONE_1/src/versione_2/Bacheca.java
@@ -0,0 +1,42 @@
+package versione_2;
+
+import java.util.ArrayList;
+
+public class Bacheca {
+
+	private ArrayList<Proposta> proposte;
+	
+	public Bacheca() {
+		this.proposte = new ArrayList<Proposta>();
+	}
+	
+	/**Metodo che permette di aggiungere una proposta alla bacheca
+	 * 
+	 * @param p la proposta da aggiungere
+	 */
+	public void aggiungiProposta (Proposta p) {
+		proposte.add(p);
+	}
+	
+	//da aggiungere controllo if per vedere se l'utente è creatore della proposta
+	public void iscrizioneProposta(int n, Utente u) {
+		proposte.get(n).aggiungiPartecipante(u);
+	}
+	
+	//da aggiungere controllo if per vedere se l'utente è creatore della proposta
+	public void rimuoviProposta(int n) {
+		proposte.remove(n);
+	}
+	/**
+	 * Metodo che permette la rimozione dalla bacheca delle proposte concluse o fallite.
+	 */
+	public void pulizia() {
+		for(int i = 0; i < proposte.size(); i++) {
+			if (p.get(i).getStato == CONCLUSA || p.get(i).getStato == FALLITA)
+					proposte.remove(i);		}
+	}
+	
+	public String toString() {
+		
+	}
+}
